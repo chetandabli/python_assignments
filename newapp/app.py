a608dd212a5f427e7da8b174a3e9b03cd153e1a2
chetandabli <hrku002@gmail.com>
2023-07-03 17:37:15 +0530

    basic flask app

--- newapp/app.py
+++ newapp/app.py
@@ -1,9 +1,5 @@
-from flask import Flask
+from flask import Flask, request, render_template
 app = Flask(__name__)
-
-@app.route('/')
-def green():
-    return "Hello World!"
 
 @app.route('/greet/<name>')
 def name(name = None):
@@ -11,6 +7,53 @@ def name(name = None):
 @app.route('/farewell/<x>')
 def bye(x = None):
     return "Goodbye, {}!".format(x)
+
+data = {}
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/create', methods=['POST'])
+def create():
+    global data
+    if request.method == 'POST':
+        x = request.get_json()
+        keys = x.keys()
+        
+        key = list(keys)[0]
+        value = x[key]
+        data[key] = value
+        return 'Entry created successfully!'
+    return data
+
+@app.route('/read')
+def read():
+    global data
+    return data
+
+@app.route('/update', methods=['PATCH'])
+def update():
+    if request.method == 'PATCH':
+        x = request.get_json()
+        keys = x.keys()
+        key = list(keys)[0]
+
+        if key in data:
+            value = x[key]
+            data[key] = value
+            return 'Entry updated successfully!'
+        else:
+            return 'Entry does not exist.'
+
+@app.route('/delete/<key>', methods=["DELETE"])
+def delete(key):
+    if request.method == 'DELETE':
+        if key in data:
+            del data[key]
+            return 'Entry deleted successfully!'
+        else:
+            return 'Entry does not exist.'
 
 
 if __name__ == '__main__':
